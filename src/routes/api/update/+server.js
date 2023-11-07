bacd288acad1dd3614a4942261ed03ff87f0d90d
AkashDeep000 <akashdeepdas970@gmail.com>
2023-11-07 21:03:59 +0530

    update

---
+++ src/routes/api/update/+server.js
@@ -0,0 +1,22 @@
+import { json } from '@sveltejs/kit'
+import gitSync from "$lib/server/gitSync.js";
+import scrape from "$lib/server/scrape.js"
+
+// /api/update GET
+
+export async function GET(event) {
+    try {
+        const data = await scrape();
+        const { changelogs } = await gitSync(data);
+        
+          const options = {
+            status: 200
+          }
+          return new Response('Success', options)  
+    } catch (error) {
+        const options = {
+            status: 500
+          }
+          return new Response('Fail', options)
+    }
+}
